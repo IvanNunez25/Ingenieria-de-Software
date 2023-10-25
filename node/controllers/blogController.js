6f6b0be81a78556c88161e568a2986693ba0a0f4
IvanNunez25 <ivan25606@gmail.com>
2023-10-25 09:22:11 -0600

    Métodos en blogController

---
+++ node/controllers/blogController.js
@@ -0,0 +1,61 @@
+import blogModel from './models/blogModel'
+
+/* Métodos del CRUD */
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await blogModel.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.json({ 'mensaje': error.message });
+  }
+}
+
+// Mostrar un registro
+export const getBlog = async (req, res) => {
+  try {
+    const blog = blogModel.findAll({
+      where: {id: req.params.id}
+    })
+    res.json(blog);
+  } catch (error) {
+    res.json({ 'mensaje': error.message });
+  }
+}
+
+// Crear un registro
+export const createBlog = async (req, res) => {
+  try {
+    await blogModel.create(req.body);
+    res.json({
+      'mensaje': 'Registro creado correctamente'
+    })
+  } catch (error) {
+    res.json({ 'mensaje' : error.message });
+  }
+}
+
+export const updateBlog = async (req, res) => {
+  try {
+    blogModel.update(req.body, {
+      where: {id: req.params.id}
+    });
+    res.json({
+      'mensaje': 'Registro actualizado correctamente'
+    })
+  } catch (error) {
+      res.json({ 'mensaje' : error.message });
+  }
+}
+
+export const deleteBlog = async (req, res) => {
+  try {
+    blogModel.destroy({
+      where: {id: req.params.id}
+    });
+    res.json({
+      'mensaje': 'Registro eliminado correctamente'
+    })
+  } catch (error) {
+      res.json({ 'mensaje' : error.message });
+  }
+}
