28c38c53a9e8ba187305506049ca910f81d4d9ed
Ivanovicx Nuñez <ivan25606@gmail.com>
2023-10-30 16:12:28 -0600

    BD + backend

--- node/controllers/casosController.js
+++ node/controllers/casosController.js
@@ -1,3 +1,4 @@
+import blogModel from '../models/blogModel.js';
 import { casosModelo, estadosModelo, municipiosModelo } from '../models/casosModelo.js';
 import { Sequelize } from "sequelize";
 
@@ -20,17 +21,7 @@ export const getAllCasos = async (req, res) => {
           attributes: ['nombre_mun'],
           where: {
             id: Sequelize.col('casos.mun')
-          },
-          // include: [
-          //   {
-          //     model: estadosModelo,
-          //     as: 'estado_association', 
-          //     attributes: [],
-          //     where: {
-          //       id: Sequelize.col('municipios.estado')
-          //     },
-          //   }
-          // ]
+          }
         }
       ],
       where: {
@@ -46,3 +37,72 @@ export const getAllCasos = async (req, res) => {
     res.json({ 'mensaje': error.message });
   }
 };
+
+// Obtener un registro
+
+export const getCaso = async(req, res) => {
+  try {
+    const caso = await casosModelo.findAll({
+      attributes: ['fecha_defuncion', 'fecha_registro', 'sexo', 'edad', 'id', 'entidad', 'mun'],
+      include: [
+        {
+          model: estadosModelo,
+          attributes: ['nombre_estado'],
+          where: {
+            id: Sequelize.col('casos.entidad'),
+          }         
+        },
+        {
+          model: municipiosModelo,
+          as: 'municipio_association',
+          attributes: ['nombre_mun'],
+          where: {
+            id: Sequelize.col('casos.mun')
+          }
+        }
+      ],
+      where: {
+        id: req.params.id
+      },
+      order: [['id', 'ASC']],
+    })
+    res.json(caso[0]);
+  } catch (error) {
+    res.json({'mensaje': error.message});
+  }
+}
+
+// Crear un registro
+export const createCaso = async (req, res) => {
+  try {
+    await casosModelo.create(req.body);
+    res.json({
+      'mensaje': 'Registro creado correctamente'
+    })
+  } catch (error) {
+    res.json({ 'mensaje': error.menssage });
+  }
+}
+
+// Actualizar un registro
+export const updateCaso = async(req, res) => {
+  try {
+    await casosModelo.update(req.body, {
+      where: {id: req.params.id}
+    });
+    res.json({ 'mensaje': 'Registro actualizado correctamente' })
+  } catch (error) {
+    res.json({'mensaje': error.message});
+  }
+}
+
+export const deleteCaso = async(req, res) => {
+  try {
+    await casosModelo.destroy({
+      where: {id: req.params.id}
+    });
+    res.json({ 'mensaje': 'Registro eliminado correctamente' });
+  } catch (error) {
+    res.json({ 'mensaje': error.message });
+  }
+}
--- node/routes/routes.js
+++ node/routes/routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 //import { createBlog, deleteBlog, getAllBlogs, getBlog, updateBlog } from '../controllers/blogController.js';
-import { getAllCasos } from '../controllers/casosController.js';
+import { getAllCasos, getCaso, createCaso, updateCaso, deleteCaso } from '../controllers/casosController.js';
 const router = express.Router();
 
 router.get('/', getAllCasos);
-// router.get('/:id', getBlog);
-// router.post('/', createBlog);
-// router.put('/:id', updateBlog);
-// router.delete('/:id', deleteBlog);
+router.get('/:id', getCaso);
+router.post('/', createCaso);
+router.put('/:id', updateCaso);
+router.delete('/:id', deleteCaso);
 
 export default router;
