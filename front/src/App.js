2b12d8f7dda4040ea08fcd7d4f7840e10f86e3cb
Ivanovicx Nuñez <ivan25606@gmail.com>
2023-11-03 08:33:53 -0600

    graficas

--- front/src/App.js
+++ front/src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import ComponenteShowPrueba from './prueba/ShowPrueba';
 import ComponenteCreatePrueba from './prueba/CreatePrueba';
 import ComponenteEditPrueba from './prueba/EditPrueba';
+
+import DatosPage from './graficas/DatosPage';
+import Inicio from './graficas/Inicio';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<ComponenteShowPrueba />} />
+          {/* <Route path='/' element={<ComponenteShowPrueba />} />
           <Route path='/create' element={<ComponenteCreatePrueba />} />
-          <Route path='/edit/:id' element={<ComponenteEditPrueba />} />
+          <Route path='/edit/:id' element={<ComponenteEditPrueba />} /> */}
+          <Route path="/" element={<Inicio />} />
+          <Route path="/datos" element={<DatosPage />} />
         </Routes>
       </BrowserRouter>
     </div>
